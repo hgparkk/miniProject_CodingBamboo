8e2b18bb6cccdbc8d910b45483327c7d21cab97e
hgparkk <127173002+hgparkk@users.noreply.github.com>
2024-10-17 17:30:27 +0900

    코드 병합 (박현규)

--- miniProject/src/main/java/com/codingbamboo/miniproject/electric/web/ElectricController.java
+++ miniProject/src/main/java/com/codingbamboo/miniproject/electric/web/ElectricController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ElectricController {
 	
+	// 전력략 대비 탄소배출량 계산 페이지 이동
 	@RequestMapping(value="/electricCalculation", method=RequestMethod.GET)
 	public String electricCalculation() {
 		return "electric/electricCalculationView";
